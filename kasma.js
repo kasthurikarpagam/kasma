4c688dda170eb8bc2ec0292cda291bb304da0688
KASTHURI <kasthurikarpagam2000@gmail.com>
2024-05-11 00:38:27 -0930

    first program

---
+++ kasma.js
@@ -0,0 +1,184 @@
+//variables
+ /*var msg="hello";
+ var msg="this is kasmaa "+msg+"bye"; 
+ alert("hello this is js");
+ console.log("hello my name is js");*/
+
+ /*let person={
+    name:"kasthuri",
+    age:24,
+    gender:"female",
+    address:"chennai,tamilnadu,,india",
+    siblings:{
+        brother:"hari",
+        sister:"sagi"
+    }
+ }//object  
+ //dot notation
+  console.log(person.siblings.brother);
+  person.age=25;
+  console.log(person.age);
+  console.log(person['gender']);
+let favcolors=["red","black","white"];
+console.log(favcolors[2]);
+favcolors[4]="bule";
+console.log(favcolors.length);
+function user(firstname,lastname){
+    
+    let message="hello"+" "+firstname+lastname+" "+"i learning js subjects" 
+    console.log(message);
+}user("div","ya")
+user("kassu","ma ")
+
+function add(number1,number2){
+    calci=number1+number2;
+    console.log(calci);
+}
+add(5,15);
+let age1=24
+let age2=age1>18?"vote":"notvote";
+console.log(age2);
+
+current_color =" nivebule";
+default_color="white";
+present_color=current_color||default_color;
+console.log(present_color); 
+
+let mark=80;
+switch(true){
+    case mark>80:
+     console.log("very good");
+         break;
+    case mark<80:
+        console.log("good"); 
+        break;
+    case mark<=35:
+        console.log("fail");
+        break;  
+        
+}
+for(let i=0;i<5;i++)
+    {
+    if(i%2!==0){
+       console.log("this is order of number"+i);
+    }
+    
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
